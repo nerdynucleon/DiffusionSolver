711c5a85f8fc881c493582bf988b82b6b6d41e2a
Alexandre Chong <alexychong@gmail.com>
2014-04-07 22:17:10 -0700

    added main

---
+++ transportsolver.f90
@@ -0,0 +1,16 @@
+PROGRAM Transport_Solver
+	IMPLICIT NONE
+	USE input
+! Accept Data From User
+	user_input()
+	WRITE(*,*) solution_method
+	READ(*,*)
+! Error Check User Input
+
+! Matrix Generation
+
+! Matrix Solving
+
+! Display Resluts
+
+END PROGRAM Transport_Solver
